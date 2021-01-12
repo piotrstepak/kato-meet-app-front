29de7861dfacde3cf7e7612849d4c3adb4d0038f
piotrstepak <piotrstepak@gmail.com>
2021-01-12 21:03:11 +0100

    Add user actions logic Two options like or dislike and specific behavior depending on the action type.

--- src/components/PersonCard/PersonCard.js
+++ src/components/PersonCard/PersonCard.js
@@ -1,41 +1,112 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './PersonCard.css';
 import Person from './Person';
 import WaitingRoomCard from "./WaitingRoomCard";
-// import data from '../../data.json';
+import { useSelector } from 'react-redux';
+import axios from "axios";
 
-function PersonCard({ data }) {
-    // jako props user i ogladajac karty pomijam swoje id/ majac dane w storze usuwam z listy swojego(dodaje do login details) i lece wtedy po kolei
-    const [people, setPeople] = useState(data);
-    console.log(people)//pomocniczy
+const setEmptyUser = {
+    "likedUsers": [],
+    "dislikedUsers": [],
+    "likedBy": [],
+    "_id": "",
+    "name": "",
+    "image": "",
+    "age": null,
+    "email": "",
+    "password": "",
+    "createdAt": "",
+    "updatedAt": "",
+}
+//refresh component eg when !people
 
-    const removePersonFromData = (people, id) => {
-        return people.filter(person => person.id !== id);
+function PersonCard() {
+    const [personToDisplay, setPersonToDisplay] = useState(setEmptyUser);
+    const loggedUser = useSelector(state => state.loginLogout.user);//consider store with hooks
+    const nextUserToDisplayAPI = 'http://localhost:3001/nextUserToDisplay/';
+
+    useEffect(() => {
+        axios.get(nextUserToDisplayAPI)
+            .then(res => {
+                setPersonToDisplay(res.data)
+                // console.log(personToDisplay)
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, []);//?personToDisplay
+
+    const fetchData = async () => {
+        const response = await axios.get(nextUserToDisplayAPI);
+        setPersonToDisplay(response.data)
+    }
+
+    const updateLoggedUserInDB = (action) => {
+        //fetch logged likedBy arr because in the meantime someone might have liked him
+        const updateUserAPI = `http://localhost:3001/users/update/${loggedUser._id}`;
+        let updatedLoggedUser = {...loggedUser};
+        console.log('Update: ')//helper print
+        console.log(updatedLoggedUser)//helper print
+        switch (action) {
+            case 'like':
+                updatedLoggedUser.likedUsers.push(personToDisplay._id);// ? first update in store and then clone it and send to db
+                loggedUser.likedUsers.push(personToDisplay._id);
+                // updateLastViewedUserInDB();
+                break;
+            case 'dislike':
+                // updatedLoggedUser = loggedUser.dislikedUsers.push(loggedUser.id)
+                updatedLoggedUser.dislikedUsers.push(personToDisplay._id);
+                loggedUser.dislikedUsers.push(personToDisplay._id);
+                break;
+            default:
+                return personToDisplay;
+        }
+
+        axios.post(updateUserAPI, updatedLoggedUser)
+            .then(res => console.log(res.data))
+            // .catch(err => res.status(400).json(`Error: ${err}`))
+    }
+
+    const updateLastViewedUserInDB = () => {
+        const updateUserAPI = `http://localhost:3001/users/update/${personToDisplay._id}`;
+        let lastViewedUser = {...personToDisplay};
+        lastViewedUser.likedBy.push(loggedUser._id);
+        console.log('Last: ')//helper print
+        console.log(lastViewedUser)//helper print
+        axios.post(updateUserAPI, lastViewedUser)
+            .then(res => console.log(res.data))
+        // .catch(err => res.status(400).json(`Error: ${err}`))
     }
 
     const handleButtonAction = (id, action) => {
         switch (action) {
             case 'ADD_TO_LIKED_USERS':
-                setPeople(removePersonFromData(people, id))
+                // updateLoggedUserInDB('like');
+                // updateLastViewedUserInDB();
+                setPersonToDisplay(setEmptyUser);
+                fetchData();//?? how to fetch from db when a new person registers ?
                 break;
             case 'ADD_TO_DISLIKED_USERS':
-                setPeople(removePersonFromData(people, id))
+                // updateLoggedUserInDB('dislike');
+                // updateLastViewedUserInDB();
+                setPersonToDisplay(setEmptyUser);
+                fetchData();
                 break;
             default:
-                return people;
+                return personToDisplay;
         }
     }
 
-    //ifa jesli id jak w storze to pominac / majac dane w storze usuwam z listy swojego (dodaje do login details) i lece wtedy po kolei
     return (
         <div className='personCard'>
-            {people[1] ? (
+            {personToDisplay._id ? (
                 <Person
-                    key={people[1].id}
-                    person={people[1]}
+                    key={personToDisplay._id}
+                    person={personToDisplay}
                     handleButtonAction={handleButtonAction}
                 />
-            ) : <WaitingRoomCard />}
+            ) : <WaitingRoomCard />
+            }
         </div>
     );
 }
