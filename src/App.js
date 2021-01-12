f2abdcb6b4d7531f7706d603853c499a11c3d4d1
piotrstepak <piotrstepak@gmail.com>
2021-01-12 20:45:56 +0100

    Implement routing

--- src/App.js
+++ src/App.js
@@ -3,19 +3,46 @@ import './App.css';
 import HamburgerMenu from "./components/HamburgerMenu/HamburgerMenu";
 import LoginCard from "./components/LoginCard/LoginCard";
 import PersonCard from "./components/PersonCard/PersonCard";
-import data from './data.json';
+import NotFound from "./components/NotFound/NotFaund";
 import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from "./components/LoginCard/Login";
+import Start from "./components/LoginCard/Start";
+import Register from "./components/LoginCard/Register";
+import Matches from "./components/Matches/Matches";
 
-//pobieranie danych-data
 function App() {
     const isLogged = useSelector(state => state.loginLogout.isLogged)
 
     return (
-        <div className='app'>
-            <HamburgerMenu isLogged={isLogged}/>
-            {isLogged ? <PersonCard data={data}/> : <LoginCard data={data}/>}
-        </div>
-    );
+        //TODO do hamburgera link do klikniecia i wyswietlenia
+        <Router>
+            <div className='app'>
+                <HamburgerMenu isLogged={isLogged}/>
+                <Switch>
+                    <Route exact path='/' component={Start} />
+                    <Route path='/login' component={Login} />
+                    <Route path='/register' component={Register} />
+                    <Route path='/logged' component={PersonCard} />
+                    {/*<Route path='/mission' component={Mission} />*/}
+                    {/*<Route path='/how-to-start' component={HowToStart} />*/}
+                    {/*<Route path='/app' component={Application} />*/}
+                    {/*<Route path='/contact' component={Contact} />*/}
+                    <Route path='/matches' component={Matches} />
+                    {/*<Route path='/messages' component={Messages} />//id z dana osoba*/}
+                    {/*<Route path='/events' component={Events} />*/}
+                    {/*<Route path='/my-profile' component={MyProfile} />*/}
+                    {/*<Route path='/more' component={More} />*/}
+                    <Route component={NotFound} />
+                </Switch>
+            </div>
+        </Router>
+    )
+    //     <div className='app'>
+    //         <HamburgerMenu isLogged={isLogged}/>
+    //         {isLogged ? <PersonCard data={data}/> : <LoginCard data={data}/>}
+    //     </div>
+    // );
 }
 
 export default App;
