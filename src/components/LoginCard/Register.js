6781cffab3d144ebbec2ef9df026469c2c80a3ef
piotrstepak <piotrstepak@gmail.com>
2021-01-13 21:02:19 +0100

    Implement basic register

--- src/components/LoginCard/Register.js
+++ src/components/LoginCard/Register.js
@@ -1,53 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './LoginCard.css';
 import { Link, useHistory } from 'react-router-dom';
-import axios from 'axios';
+import AuthService from "../../services/auth.service";
 
-//isolate common parts from Login and Register component ? store ?
+//add client side validation with formik
 
 function Register() {
-    const [users, setUsers] = useState([]);
-
     const [email, setEmail] = useState('');//all register data to one object ?
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
     const [age, setAge] = useState(null);
+    // const [loading, setLoading] = useState(false);
 
     const history = useHistory();
-
-    useEffect(() => {
-        const usersAPI = 'http://localhost:3001/users/';
-
-        axios.get(usersAPI)
-            .then(res => {
-                setUsers(res.data)
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }, []);
-
-    const handleRegisterSubmit = (event) => {
-        event.preventDefault();
-        //check if the given email exists in db
-        users.filter(user => user.email === email) ?
-            alert('Podany email już istnieje, zaloguj się lub użyj innego.') :
-            alert('Email został wysłany. Prosimy potwierdzić rejestrację.')
-        //send an email to confirm registration
-
-        //if user confirms, add him to db
-        const userToAdd = {
-            name: name,
-            age: age,
-            email: email,
-            password: password,
-        }
-
-        axios.post('http://localhost:3001/users/add', userToAdd)
-            .then(res => console.log(res.data));
-
-        history.push('/');// /login
-    }
 
     const handleAgeChange = event => {
         setAge(event.target.value)
@@ -57,13 +22,24 @@ function Register() {
         setName(event.target.value)
     }
 
-    const handleEmailChange = (event) => (
+    const handleEmailChange = event => (
         setEmail(event.target.value)
     )
 
-    const handlePasswordChange = (event) => (
+    const handlePasswordChange = event => (
         setPassword(event.target.value)
     )
+
+    const handleRegisterSubmit = async event => {
+        event.preventDefault();
+        // setLoading(true);
+        try {
+            await AuthService.register(name, age, email, password);
+            history.push('/login');
+        } catch (err) {
+            alert('Podany email już istnieje, zaloguj się lub użyj innego.');
+        }
+    }
 
     return (
         //to undo the button instead of the link ?
