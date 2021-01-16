f17ead3f7514414f603451a70bc3834f01e329e1
piotrstepak <piotrstepak@gmail.com>
2021-01-16 21:58:33 +0100

    Update Use auth service to token send. Change const variable names. Add async/await in handling button action.

--- src/components/PersonCard/PersonCard.js
+++ src/components/PersonCard/PersonCard.js
@@ -4,6 +4,7 @@ import Person from './Person';
 import WaitingRoomCard from "./WaitingRoomCard";
 import { useSelector } from 'react-redux';
 import axios from "axios";
+import authHeader from "../../services/authHeader.service";
 
 const setEmptyUser = {
     "likedUsers": [],
@@ -23,74 +24,96 @@ const setEmptyUser = {
 function PersonCard() {
     const [personToDisplay, setPersonToDisplay] = useState(setEmptyUser);
     const loggedUser = useSelector(state => state.loginLogout.user);//consider store with hooks
-    const nextUserToDisplayAPI = 'http://localhost:3001/nextUserToDisplay/';
+    const NEXT_USER_TO_DISPLAY_API = 'http://localhost:3001/users/nextUserToDisplay/';
+    console.log(`ID: ${loggedUser._id}`)//helper print
 
     useEffect(() => {
-        axios.get(nextUserToDisplayAPI)
+        // const fetchPersonToDisplay = async () => {
+        //     let userToDisplay;
+        //     return userToDisplay = await axios.post(NEXT_USER_TO_DISPLAY_API, {
+        //         _id: loggedUser._id,
+        //     }, {
+        //         headers: authHeader(),
+        //     });
+        //     // setPersonToDisplay(userToDisplay);
+        // };
+        // fetchPersonToDisplay().then(r => setPersonToDisplay(r.data));//???
+        axios.post(NEXT_USER_TO_DISPLAY_API, {
+            id: loggedUser._id
+        }, {
+            headers: authHeader()
+        })
             .then(res => {
                 setPersonToDisplay(res.data)
-                // console.log(personToDisplay)
             })
-            .catch((error) => {
-                console.log(error);
+            .catch(err => {
+                console.log(err);
             })
     }, []);//?personToDisplay
 
     const fetchData = async () => {
-        const response = await axios.get(nextUserToDisplayAPI);
-        setPersonToDisplay(response.data)
+        const res = await axios.post(NEXT_USER_TO_DISPLAY_API, {
+            id: loggedUser._id
+        }, {
+            headers: authHeader()
+        })
+        // const response = await axios.get(NEXT_USER_TO_DISPLAY_API, {
+        //     headers: authHeader(),
+        //     data: loggedUser._id
+        // });
+        setPersonToDisplay(res.data);
     }
 
-    const updateLoggedUserInDB = (action) => {
+    const updateLoggedUserInDB = action => {
         //fetch logged likedBy arr because in the meantime someone might have liked him
-        const updateUserAPI = `http://localhost:3001/users/update/${loggedUser._id}`;
+        const UPDATE_USER_API = `http://localhost:3001/users/update/${loggedUser._id}`;
         let updatedLoggedUser = {...loggedUser};
         console.log('Update: ')//helper print
         console.log(updatedLoggedUser)//helper print
         switch (action) {
             case 'like':
                 updatedLoggedUser.likedUsers.push(personToDisplay._id);// ? first update in store and then clone it and send to db
-                loggedUser.likedUsers.push(personToDisplay._id);
+                // loggedUser.likedUsers.push(personToDisplay._id);
                 // updateLastViewedUserInDB();
                 break;
             case 'dislike':
                 // updatedLoggedUser = loggedUser.dislikedUsers.push(loggedUser.id)
                 updatedLoggedUser.dislikedUsers.push(personToDisplay._id);
-                loggedUser.dislikedUsers.push(personToDisplay._id);
+                // loggedUser.dislikedUsers.push(personToDisplay._id);
                 break;
             default:
                 return personToDisplay;
         }
 
-        axios.post(updateUserAPI, updatedLoggedUser)
+        axios.put(UPDATE_USER_API, updatedLoggedUser)
             .then(res => console.log(res.data))
             // .catch(err => res.status(400).json(`Error: ${err}`))
     }
 
     const updateLastViewedUserInDB = () => {
-        const updateUserAPI = `http://localhost:3001/users/update/${personToDisplay._id}`;
+        const UPDATE_USER_API = `http://localhost:3001/users/update/${personToDisplay._id}`;
         let lastViewedUser = {...personToDisplay};
         lastViewedUser.likedBy.push(loggedUser._id);
         console.log('Last: ')//helper print
         console.log(lastViewedUser)//helper print
-        axios.post(updateUserAPI, lastViewedUser)
+        axios.put(UPDATE_USER_API, lastViewedUser)
             .then(res => console.log(res.data))
         // .catch(err => res.status(400).json(`Error: ${err}`))
     }
 
-    const handleButtonAction = (id, action) => {
+    const handleButtonAction = async (id, action) => {
         switch (action) {
             case 'ADD_TO_LIKED_USERS':
-                // updateLoggedUserInDB('like');
-                // updateLastViewedUserInDB();
+                await updateLoggedUserInDB('like');
+                await updateLastViewedUserInDB();
                 setPersonToDisplay(setEmptyUser);
-                fetchData();//?? how to fetch from db when a new person registers ?
+                await fetchData();//?? how to fetch from db when a new person registers ?
                 break;
             case 'ADD_TO_DISLIKED_USERS':
-                // updateLoggedUserInDB('dislike');
-                // updateLastViewedUserInDB();
+                await updateLoggedUserInDB('dislike');
+                await updateLastViewedUserInDB();
                 setPersonToDisplay(setEmptyUser);
-                fetchData();
+                await fetchData();
                 break;
             default:
                 return personToDisplay;
@@ -99,7 +122,7 @@ function PersonCard() {
 
     return (
         <div className='personCard'>
-            {personToDisplay._id ? (
+            {personToDisplay && personToDisplay._id ? (
                 <Person
                     key={personToDisplay._id}
                     person={personToDisplay}
