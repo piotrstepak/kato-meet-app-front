3d5a4920ee96923821f049a0a15d7f7c0e956a42
piotrstepak <piotrstepak@gmail.com>
2021-01-21 18:16:13 +0100

    Add link with logo

--- src/components/HamburgerMenu/buttons/Logo.js
+++ src/components/HamburgerMenu/buttons/Logo.js
@@ -1,9 +1,12 @@
 import React from "react";
+import logo from '../../../resources/images/logos/logo_two_colors_long.png'
+import { Link } from "react-router-dom";
 
 const Logo = () => (
-    <button>
-        LOGO
-    </button>
+    <Link to='/logged'>
+        <img src={logo} className='logo-img' alt='logo' width="100" height="50"  />
+    </Link>
+
 )
 
 export default Logo;
