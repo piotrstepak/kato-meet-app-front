a05d2c040f390b441936a88c38358ba8abb365ec
piotrstepak <piotrstepak@gmail.com>
2021-01-28 18:04:55 +0100

    Add LoadingSpinner component Use spinner in Login and Matches

---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import StyledLoadingSpinner from './styled/StyledLoadingSpinner';
+
+const LoadingSpinner = () => (
+    <StyledLoadingSpinner>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </StyledLoadingSpinner>
+)
+
+export default LoadingSpinner;
