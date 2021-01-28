9d93d75c74130d3b65bf41e5bb586cb0e41d11f6
piotrstepak <piotrstepak@gmail.com>
2021-01-28 20:39:54 +0100

    Add scroll and more options to user update

---
+++ src/components/MyProfile/styled/StyledCheckBox.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+const StyledCheckBox = styled.div`
+    //display: flex;
+    padding: 0 0 10px;
+    text-align: center;
+    //input
+    //justify-content: center;
+    //align-items: center;
+    //label {
+    //cursor: pointer;
+    //}
+    /* On mouse-over, add a grey background color */
+    //justify-content: ;
+    label {
+      padding: 10px;
+      
+        //input {
+        //padding: 5px;
+        //}
+        //
+        //span {
+        //background-color: red;
+        ////color: red;
+        //}
+    }
+    
+    //& input {
+    //padding: 0 0 0 3px;
+    //}
+    //span {
+    //background-color: red;
+    //color: red;
+    //}
+    //input {
+    //background-color: red;
+    //color: red;
+    //}
+    //
+`;
+
+export default StyledCheckBox;
